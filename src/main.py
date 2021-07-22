3a126449801485fca68b618a6c3a0eaaa06571f4
Robert Spencer <rspencer@lbsd.net>
2021-07-22 18:56:36 +0200

    Use a proper parser
    This also fixes 2 bugs, CDATA is no longer saved with the text and all
    the tags are saved properly too.

--- src/main.py
+++ src/main.py
@@ -1,87 +1,55 @@
 import os
+import feedparser
 
 
 def parseData():
-    input_file = open("personal_data.xml", "r")
-    content = input_file.read()
+    content = feedparser.parse("personal_data.xml")
 
     output_date_file = open("dateTime.txt", "w")
     file_count = 0
     tag_count = 0
 
-    while True:
-        if "<item>" in content:
-            file_count += 1
-            output_text_file = open("files/" + str(file_count) + ".txt", "w")
-            print(file_count)
+    for item in content.entries:
+        file_count += 1
+        output_text_file = open("files/" + str(file_count) + ".txt", "w")
+        print(file_count)
 
-            # print(len(content))
-            file_content = content[content.index("<item>") : content.index("</item>")]
-            # print(file_content)
+        # print(item)
 
-            try:
-                title = file_content[
-                    file_content.index("<title>") + 7 : file_content.index("</title>")
-                ]
-            except ValueError:
-                title = ""
-            print(title)
+        title = item.title
+        print(title)
 
-            try:
-                file_content = file_content[file_content.index("</title>") :]
-            except ValueError:
-                file_content = file_content[file_content.index("<title/>") :]
-            text = file_content[
-                file_content.index("<content:encoded>")
-                + 26 : file_content.index("</content:encoded>")
-                - 3
-            ]
-            # print(text)
+        text = item.content[0]["value"]
+        # print(text)
 
-            file_content = file_content[file_content.index("</content:encoded>") :]
-            dateTime = file_content[
-                file_content.index("<wp:post_date_gmt>")
-                + 18 : file_content.index("</wp:post_date_gmt>")
-            ]
-            # print(dateTime + "\n")
+        dateTime = item.wp_post_date_gmt
+        # print(dateTime + "\n")
 
-            file_content = file_content[file_content.index("</wp:post_date_gmt>") :]
-
-            tags = []
-            while '<category domain="post_tag"' in file_content:
-                file_content = file_content[
-                    file_content.index('<category domain="post_tag"') :
-                ]
-                tags.append(
-                    file_content[
-                        file_content.index("<![CDATA")
-                        + 9 : file_content.index("</category>")
-                        - 3
-                    ]
-                )
-                tag_count += 1
-
-                file_content = file_content[file_content.index("</category>") :]
-
+        tags = []
+        try:
+            for tag in item.tags:
+                if tag["scheme"] == "post_tag":
+                    tags.append(tag["term"])
+                    tag_count += 1
             # print(tags)
 
             tags_string = " ".join(["#" + i for i in tags])
             # print(tags_string)
+        except AttributeError:
+            pass
 
-            output_text_file_content = title + "\n\n" + text + "\n\n" + tags_string
-            # print(output_text_file_content)
+        output_text_file_content = title + "\n\n" + text + "\n\n" + tags_string
+        # print(output_text_file_content)
 
-            output_date_file.write(dateTime + "\n")
-            output_text_file.write(output_text_file_content)
-            output_text_file.close()
+        output_date_file.write(dateTime + "\n")
+        output_text_file.write(output_text_file_content)
+        output_text_file.close()
 
-            content = content[content.index("</item>") + 5 :]
-            print("\n..................\n")
-        else:
-            output_date_file.close()
-            print(file_count)
-            print(tag_count)
-            break
+        print("\n..................\n")
+
+    output_date_file.close()
+    print(file_count)
+    print(tag_count)
 
 
 def makeDayOneEntries():
